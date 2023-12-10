79c64f617801baae5e2a246b8eb32d42e996f152
Ryan Zemlin <rzemlin@gmail.com>
2023-12-10 12:30:37 -0800

    add global and local css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      {/* <h2>Header</h2> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="app" element={<AppLayout />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
